1bda4ac45bfaa37914fa08745cbe0a8a9d2bf735
Davide Gessa <gessadavide@gmail.com>
2017-08-30 09:50:14 +0200

    fix letchain import

--- src/letlightd.ml
+++ src/letlightd.ml
@@ -3,8 +3,10 @@ open Protocol;;
 
 
 let main () =
-	let lc = Letchain.init ~loglevel:0 (PrunedNode (1024)) in
-  Letchain.is_synchronized lc
+	let lc = Letchain.init ~loglevel:3 (PrunedNode (1024)) in
+  Letchain.is_synchronized lc;
+
+  Letchain.join_threads lc
 ;;
 
 
